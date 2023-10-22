6af1d4e96e9886d7328fdbe1095a8b9f87780023
Jesse Lee Ross <jesseross001@gmail.com>
2023-10-22 11:34:41 +0000

    Added operand 1

--- script.js
+++ script.js
@@ -1,74 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
+  let operand1 = ""; // Initialize operand1 as an empty string
 
-let one = document.getElementById ("one")
-let two = document.getElementById ("two");
-let three = document.getElementById ("three");
-let four = document.getElementById ("four");
-let five = document.getElementById ("five");
-let six = document.getElementById ("six");
-let seven = document.getElementById ("seven");
-let eight = document.getElementById ("eight");
-let nine = document.getElementById ("nine");
-let zero = document.getElementById ("zero");
+  let display = document.getElementById("display-port");
 
-let one1 =one.addEventListener('click',digitClicks(event));
-let two1= two.addEventListener('click',digitClicks(event));
-let three1 = three.addEventListener('click',digitClicks(event));
-let four1 =four.addEventListener('click',digitClicks(event));
-let five1= five.addEventListener('click',digitClicks(event));
-let six1 =six.addEventListener('click',digitClicks(event));
-let seven1 =seven.addEventListener('click',digitClicks(event));
-let eight1 =eight.addEventListener('click',digitClicks(event));
-let nine1 = nine.addEventListener('click',digitClicks(event));
-let zero1 = zero.addEventListener('click',digitClicks(event));
+  // Add click event listeners for digits
+  document.getElementById("one").addEventListener('click', function() {
+    digitClicks('1');
+  });
+  document.getElementById("two").addEventListener('click', function() {
+    digitClicks('2');
+  });
+  document.getElementById("three").addEventListener('click', function() {
+    digitClicks('3');
+  });
+  document.getElementById("four").addEventListener('click', function() {
+    digitClicks('4');
+  });
+  document.getElementById("five").addEventListener('click', function() {
+    digitClicks('5');
+  });
+  document.getElementById("six").addEventListener('click', function() {
+    digitClicks('6');
+  });
+  document.getElementById("seven").addEventListener('click', function() {
+    digitClicks('7');
+  });
+  document.getElementById("eight").addEventListener('click', function() {
+    digitClicks('8');
+  });
+  document.getElementById("nine").addEventListener('click', function() {
+    digitClicks('9');
+  });
+  document.getElementById("zero").addEventListener('click', function() {
+    digitClicks('0');
+  });
 
-operand1 ="";
-
-function digitClicks(event) { 
-// When i click a button i want to display it in a string .
-if (one1 == true ) {
-  operand1 += "1";
-}
-else if (two1 == true ) {
-  operand1 += "2";
-}
-
-else if (three1 == true ) {
-  operand1 += "3";
-}
-
-else if (four1 == true ) {
-  operand1 += "4";
-}
-
-else if (five1 == true ) {
-  operand1 += "5";
-}
-
-else if (six1 == true ) {
-  operand1 += "6";
-}
-
-else if (seven1 == true ) {
-  operand1 += "7";
-}
-
-else if (eight1 == true ) {
-  operand1 += "8";
-}
-  
-else if (nine1 == true ) {
-  operand1 += "9";
-}
-
-else if (zero1 == true ) {
-  operand1 += "0";
-}
-
-console.log(operand1) ;
-
-
-}
+  // Handle digit clicks and update the display
+  function digitClicks(digit) {
+    operand1 += digit;
+    display.innerText = operand1; // Update the display with operand1
+  }
 });
 
 
